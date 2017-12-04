27ebe694bfd4308eb65c71336e866063909b2773
WalkWithWind <956035125@qq.com>
2017-12-04 08:53:08 +0800

    Initial Commit

---
+++ CoreGraphic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreGraphic
+//
+//  Created by sycf_ios on 2017/12/4.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreGraphic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreGraphic
+//
+//  Created by sycf_ios on 2017/12/4.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
